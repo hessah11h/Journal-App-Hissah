519020e2bce0c6980bb6b527ab9b4565916e2c08
hessah11h <hussah.alqusi@gmail.com>
2024-10-20 15:28:28 +0300

    Initial Commit

---
+++ Journal App/Journal_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_AppApp.swift
+//  Journal App
+//
+//  Created by hussah alqusi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Journal_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Journal AppTests/Journal_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_AppTests.swift
+//  Journal AppTests
+//
+//  Created by hussah alqusi on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Journal_App
+
+struct Journal_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
